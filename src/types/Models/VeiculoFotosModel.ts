9af036f822c9ad6be6ab9074a2f383afffe59734
alandas71 <alanalves.dev@gmail.com>
2024-10-01 14:35:44 -0300

    Fix: colocado o restante das situacoes de Situacao

--- src/types/Models/VeiculoFotosModel.ts
+++ src/types/Models/VeiculoFotosModel.ts
@@ -2,7 +2,7 @@ export interface Veiculo_Fotos {
   id?: number;
   Veiculo_id: number;
   Foto_url: string;
-  Situacao: string; // checado / checar
+  Situacao: string; // checado / checando / checar
   Criado_em?: Date;
   Atualizado_em?: Date;
 }
